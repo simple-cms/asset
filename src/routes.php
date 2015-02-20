5fb662f32d95cc00a1bbd57f423afec545d30456
Tom Green <kingofcuddles@gmail.com>
2015-02-20 17:50:02 +0000

    working on glide image handling..
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/routes.php
+++ src/routes.php
@@ -4,3 +4,16 @@ Route::group(['prefix' => config('core.adminURL')], function()
 {
   Route::resource(config('asset.assetURL'), 'SimpleCms\Asset\AdminController');
 });
+
+Route::get('asset/{slug}', function ($request)
+{
+  $glide = League\Glide\ServerFactory::create([
+    'source' => public_path() .'/assets/images',
+    'cache' => storage_path() . '/framework/cache/images',
+    'max_image_size' => 2560*2000,
+    'base_url' => '/asset/',
+  ]);
+
+  $glide->outputImage($request)->getSourcePath();
+});
+
